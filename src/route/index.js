19b1aca2f0b4bc6c35384a244729e034ff990742
BobKii <two.heap2008@gmail.com>
2023-12-12 21:57:02 +0200

    12_12_23_verstka_html_css bez JS proekt JS-layout

--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,17 @@
 // Підключаємо роутер до бек-енду
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Підключіть файли роутів
-const test = require('./test')
+const test = require("./test");
+
+const comuniti = require("./comuniti");
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use("/", test);
+router.use("/comuniti", comuniti);
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
-module.exports = router
+module.exports = router;
